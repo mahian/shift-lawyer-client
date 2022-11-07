e1db096c93933fc94ecf18ec95673b81fb723d93
mahian ahmad <mahianahamd9@gmail.com>
2022-11-08 01:41:43 +0600

    add services

--- src/components/Services.js
+++ src/components/Services.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import SectionTitle from './SectionTitle';
-import { GiClawHammer } from 'react-icons/gi';
 import { Link } from 'react-router-dom';
+import { Button } from '@material-tailwind/react';
 
 const Service = () =>{
     return (
         <div className='shadow-md p-8 rounded-lg'>
-            <GiClawHammer className='text-4xl text-light-blue-500'/>
+            <img className='h-52 w-full rounded-lg' src="https://img.freepik.com/premium-photo/lawyer-judge-counselor-having-team-meeting-with-client-law-legal-services_28283-884.jpg?w=2000" alt="" />
             <h2 className='text-3xl font-bold mt-3'>Civil Litigation</h2>
             <p className='mt-5 text-gray-500'>Claritas est etiam processus dynamicus, qui sequitur mutationem consuetudium lectorum.</p>
+            <Link to="service-details"><Button className='mt-5 w-full' variant="gradient">Learn More</Button></Link>
         </div>
     );
 }
@@ -18,12 +19,12 @@ const Services = () => {
         <section className='my-20 container mx-auto'>
             <SectionTitle title="Our services"/>
             <div className='grid md:grid-cols-3 gap-10'>
-                <Link to="service-details"><Service/></Link>
-                <Link to="service-details"><Service/></Link>
-                <Link to="service-details"><Service/></Link>
-                <Link to="service-details"><Service/></Link>
-                <Link to="service-details"><Service/></Link>
-                <Link to="service-details"><Service/></Link>
+                <Service/>
+                <Service/>
+                <Service/>
+                <Service/>
+                <Service/>
+                <Service/>
             </div>
         </section>
     );
