df16bc239c54aa9074968565029202b20307a497
mahian ahmad <mahianahamd9@gmail.com>
2022-12-19 18:23:59 +0600

    add loading spinner

--- src/components/Services.js
+++ src/components/Services.js
@@ -6,11 +6,13 @@ import ServiceItem from './ServiceItem';
 const Services = () => {
     useTitle('services')
     const [services, setServices] = useState([]);
+    const [loading, setLoading] = useState(true)
     useEffect(() => {
         const fetchData = async () => {
             try {
                 const response = await fetch('https://shift-lawyer-server.vercel.app/services');
                 const json = await response.json();
+                setLoading(false)
                 setServices(json);
             } catch (error) {
                 console.log("error", error);
@@ -19,12 +21,18 @@ const Services = () => {
 
         fetchData();
     }, [])
+
+    if (loading) {
+        return <div className='flex items-center justify-center py-10'>
+            <div class="w-12 h-12 rounded-full animate-spin border-2 border-solid border-blue-500 border-t-transparent"></div>
+        </div>
+    }
     return (
         <section className='my-20 container mx-auto'>
             <SectionTitle title="Our services" />
             <div className='grid md:grid-cols-3 gap-10'>
                 {
-                    services.map(service => <ServiceItem key={service._id} service={service}/>)
+                    services.map(service => <ServiceItem key={service._id} service={service} />)
                 }
             </div>
         </section>
